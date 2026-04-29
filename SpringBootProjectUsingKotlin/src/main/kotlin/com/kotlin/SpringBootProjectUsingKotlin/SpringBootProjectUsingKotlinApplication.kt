6cb599d1b1e74af01a9dd7d143d07a8407fcc219
Zafar imam <zafarimam7032@gmail.com>
2026-04-29 21:56:47 +0530

    kotin  basic

---
+++ SpringBootProjectUsingKotlin/src/main/kotlin/com/kotlin/SpringBootProjectUsingKotlin/SpringBootProjectUsingKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.SpringBootProjectUsingKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootProjectUsingKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootProjectUsingKotlinApplication>(*args)
+}
